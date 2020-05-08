ace135b2dff0add5a3a0559448fdb6860abcd779
Uzor Mercy <mercy4hope@gmail.com>
2020-05-08 12:00:24 +0100

    Made the next button clickable for the next question

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict'
 const domQuestion = document.querySelector('.question');
-const domAnswers = document.querySelector('.ans');
+const domAnswers = document.querySelector('.option');
+const btn = document.querySelector('.btn');
+
 
 function Question(question, answers, correct){
     this.question = question;
@@ -11,6 +13,7 @@ function Question(question, answers, correct){
 Question.prototype.displayQuestion = function (){
     domQuestion.innerHTML = this.question;
     const correctAnsewer = this.correct;
+
     //check correct answer
     const getCorrectAns = function(ans){
         let picked = ans.target.getAttribute('data-index')
@@ -21,18 +24,19 @@ Question.prototype.displayQuestion = function (){
             adCorrectColor.classList.add('wrong');
             
         }
+
     }
 
 
     //Display answers
    for (let i =0; i < this.answers.length; i++){
-    const li = document.createElement('li');
+    const span = document.createElement('span');
     let data_index = document.createAttribute("data-index");
     data_index.value = i;
-    li.setAttributeNode(data_index)
-       domAnswers.appendChild(li);
-        li.innerHTML = li.innerHTML + i + ". " + this.answers[i];
-        li.addEventListener('click', getCorrectAns)
+    span.setAttributeNode(data_index)
+       domAnswers.append(span);
+    span.innerHTML = span.innerHTML+ i + ". " + this.answers[i]
+    span.addEventListener('click', getCorrectAns)
    }
 }
 
@@ -49,6 +53,19 @@ let q3 = new Question('Is Covid-19 a scam in this Country?', ['True', 'False'], 
 
 let questions = [q1, q2, q3];
 
-//Display Random Question
-let randQuestions = Math.floor(Math.random() * questions.length);
-questions[randQuestions].displayQuestion();
+let tab = 0;
+
+questions[tab].displayQuestion();
+btn.addEventListener('click', function(){
+    questions.forEach((item, indx)=>{
+        tab = 1;
+
+        if(indx == tab){
+            item.displayQuestion()
+            // domQuestion.innerHTML = "";
+            return 
+        }
+    })
+    tab += 1;
+})
+
